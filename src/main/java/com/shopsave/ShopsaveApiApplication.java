37e21fca93458a9d6d4ecf6bb800d251c6fb2b95
Phan Van Vu <phanvuars@Phans-MacBook-Pro.local>
2025-01-25 10:00:56 +0700

    Init project

---
+++ src/main/java/com/shopsave/ShopsaveApiApplication.java
@@ -0,0 +1,13 @@
+package com.shopsave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopsaveApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ShopsaveApiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/shopsave/ShopsaveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopsave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopsaveApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
